fe876091f0bd4ca0cdd3653f831e7da63d3a15a2
jmalkin <786705+jmalkin@users.noreply.github.com>
2024-12-02 17:23:18 -0800

    Remove codegen attempts, test now kind of works

--- src/main/scala/org/apache/spark/sql/expressions/KllExpressions.scala
+++ src/main/scala/org/apache/spark/sql/expressions/KllExpressions.scala
@@ -5,7 +5,6 @@ import org.apache.datasketches.kll.KllDoublesSketch
 import org.apache.spark.sql.catalyst.expressions.{Expression, ExpectsInputTypes, UnaryExpression}
 import org.apache.spark.sql.types.{AbstractDataType, DataType, DoubleType, KllDoublesSketchType}
 import org.apache.spark.sql.catalyst.expressions.NullIntolerant
-import org.apache.spark.sql.catalyst.expressions.codegen.{CodeBlock, CodegenContext, ExprCode}
 import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
 
 case class KllGetMin(child: Expression)
@@ -27,28 +26,11 @@ case class KllGetMin(child: Expression)
     val sketch = KllDoublesSketch.wrap(Memory.wrap(bytes))
     sketch.getMinItem
   }
-
-  /*
-  override protected def nullSafeCodeGen(ctx: CodegenContext, ev: ExprCode, f: String => String): ExprCode = {
-    val bytes = ctx.freshName("bytes")
-    val sketch = ctx.freshName("sketch")
-    val code =
-      s"""
-         |final byte[] $bytes = ${f(child.genCode(ctx).value)}.getBytes();
-         |final org.apache.datasketches.kll.KllDoublesSketch $sketch = org.apache.datasketches.kll.KllDoublesSketch.wrap(org.apache.datasketches.memory.Memory.wrap($bytes));
-         |$sketch.getMinItem();
-       """.stripMargin
-    ev.copy(code = CodeBlock(Seq(code), Seq.empty))
-  }
-
-  override def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
-    nullSafeCodeGen(ctx, ev, c => s"($c)")
-  }
-  */
 }
 
 case class KllGetMax(child: Expression)
  extends UnaryExpression
+ with CodegenFallback
  with ExpectsInputTypes
  with NullIntolerant {
 
@@ -64,25 +46,5 @@ case class KllGetMax(child: Expression)
     val bytes = input.asInstanceOf[Array[Byte]]
     val sketch = KllDoublesSketch.wrap(Memory.wrap(bytes))
     sketch.getMaxItem
-  }
-
-  override protected def nullSafeCodeGen(ctx: CodegenContext, ev: ExprCode, f: String => String): ExprCode = {
-    println("******\nChild type: " + child.toString() + "\n******")
-    val childEval = child.genCode(ctx)
-    println("******\nChild eval: " + childEval.toString() + "\n******")
-    val sketch = ctx.freshName("sketch")
-    println("******\nSketch name: " + sketch + "\n******")
-    val code =
-      s"""
-         |${childEval.code}
-         |final org.apache.datasketches.kll.KllDoublesSketch $sketch = org.apache.datasketches.kll.KllDoublesSketch.wrap(org.apache.datasketches.memory.Memory.wrap(${childEval.value}));
-         |${ev.value} = $sketch.getMaxItem();
-       """.stripMargin
-    println(s"Generated code: $code")
-    ev.copy(code = CodeBlock(Seq(code), Seq.empty), isNull = childEval.isNull)
-  }
-
-  override def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
-    nullSafeCodeGen(ctx, ev, c => s"($c)")
   }
 }
--- src/main/scala/org/apache/spark/sql/types/KllDoublesSketchWrapper.scala
+++ src/main/scala/org/apache/spark/sql/types/KllDoublesSketchWrapper.scala
@@ -2,13 +2,13 @@ package org.apache.spark.sql.types
 
 import org.apache.datasketches.kll.KllDoublesSketch
 import org.apache.datasketches.memory.Memory
-//import org.apache.datasketches.memory.{WritableMemory, DefaultMemoryRequestServer}
 
 @SQLUserDefinedType(udt = classOf[KllDoublesSketchType])
 case class KllDoublesSketchWrapper(sketch: KllDoublesSketch) extends Serializable {
-  //def this() = this(KllDoublesSketch.newHeapInstance())
 
   def toByteArray: Array[Byte] = sketch.toByteArray
+
+  def serialize: Array[Byte] = sketch.toByteArray
 
   def toString(withLevels: Boolean = false, withData: Boolean = false ): String = {
     sketch.toString(withLevels, withData)
@@ -16,16 +16,6 @@ case class KllDoublesSketchWrapper(sketch: KllDoublesSketch) extends Serializabl
 }
 
 object KllDoublesSketchWrapper {
-  //implicit val kllDoublesSketchSerializer: KllDoublesSketch => Array[Byte] = _.toByteArray
-  //implicit val kllDoublesSketchDeserializer: Array[Byte] => KllDoublesSketch = bytes => {
-  //  KllDoublesSketch.writableWrap(WritableMemory.writableWrap(bytes), new DefaultMemoryRequestServer())
-  //}
-
-  // not used now but we may want to use a generic wrapper later
-  //type KllDoublesSketchWrapper = GenericWrapper[KllDoublesSketch]
-  //val KllDoublesSketchWrapperType: GenericWrapperType[KllDoublesSketch] = GenericWrapperType[KllDoublesSketch]
-
-  // TODO: determine if heapify is better
   def deserialize(bytes: Array[Byte]): KllDoublesSketchWrapper = {
     //val sketch = KllDoublesSketch.writableWrap(WritableMemory.writableWrap(bytes), new DefaultMemoryRequestServer())
     val sketch = KllDoublesSketch.heapify(Memory.wrap(bytes))
